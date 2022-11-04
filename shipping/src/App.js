565be3ab3ea504ceb64c1f687ab935d98733d5fb
Prabu Mahendran <prabum1985@gmail.com>
2022-11-04 19:18:53 +0530

    Check validation in shipping

--- shipping/src/App.js
+++ shipping/src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React , {useState} from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Shipping from './components/Shipping/Shipping';
 import Dashboard from './components/Dashboard/Dashboard';
 import Login from './components/Login/Login';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    () => localStorage.getItem('token') !== null
+  );
   return (
     <div className="wrapper">
       <h1>Application</h1>
       <BrowserRouter>
         <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/shipping" element={<Shipping />} />
-          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={isLoggedIn ? <Dashboard/> : <Navigate to='/'/>} />
+          <Route path="/shipping" element={isLoggedIn ? <Shipping/> : <Navigate to='/'/>} />
+          <Route path="/" element={!isLoggedIn ? <Login/> : <Navigate to='/shipping'/>}/>
               
         </Routes>
       </BrowserRouter>
--- shipping/src/components/Login/Login.js
+++ shipping/src/components/Login/Login.js
@@ -1,19 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate, useParams, useNavigate } from 'react-router-dom';
+
 
 export default function Login() {
-  return(
-    <form>
-      <label>
-        <p>Username</p>
-        <input type="text" />
-      </label>
-      <label>
-        <p>Password</p>
-        <input type="password" />
-      </label>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+  const [value, setValue] = useState('');
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+  let navigate = useNavigate();
+  const [values, setValues] = useState({
+    _username: '', _password: ''
+  });
+  const login = async () => {
+    
+    const response = await fetch('https://deepbluapi.gocontec.com/login_check', {
+      method: 'POST',
+      body: new URLSearchParams(values),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+      }
+    });
+    if (response.status !== 200) {
+      throw new Error(`Request failed: ${response.status}`);
+    }
+    const token = await response.json();
+    localStorage.setItem('token', token['token'])
+    navigate('/shipping') 
+  }
+  const set = name => {
+    return ({ target: { value } }) => {
+      setValues(oldValues => ({ ...oldValues, [name]: value }));
+    }
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault(); // Prevent default submission
+    try {
+      await login();
+      setValues({
+        _username: '', _password: ''
+      });
+    } catch (e) {
+      alert(`Registration failed! ${e.message}`);
+    }
+  }
+
+  return (
+    <div class="login">
+      <form onSubmit={onSubmit}>
+        <div><h2>Login</h2></div>
+        <label>
+          <p>Username</p>
+          <input required type="text" value={values._username} onChange={set('_username')} />
+        </label>
+        <label>
+          <p>Password</p>
+          <input required type="password" value={values._password} onChange={set('_password')} />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
   )
 }
